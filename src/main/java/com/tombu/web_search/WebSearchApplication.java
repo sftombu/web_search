8d4b0777f26fab7d47b3eb45fd49a733bdb4e18b
sftombu <pub@tombu.com>
2022-01-03 17:11:56 -0800

    first checkin
    first checking

---
+++ src/main/java/com/tombu/web_search/WebSearchApplication.java
@@ -0,0 +1,13 @@
+package com.tombu.web_search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebSearchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tombu/web_search/WebSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tombu.web_search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
